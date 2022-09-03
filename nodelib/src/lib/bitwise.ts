94122e3b804adf4692e8dbdaadaa147638d87c28
cmd <cmd@yggdrasil.onion>
2022-09-03 12:44:21 -0500

    update

--- nodelib/src/lib/bitwise.ts
+++
@@ -1,8 +0,0 @@
-
-export function add(a: bigint, b: bigint): bigint {
-  if (b === 0n) {
-    return a
-  } else {
-    return add(a^b , (a&b) << 1n)
-  }
-}
--- nodelib/src/scratch.ts
+++
@@ -1,8 +0,0 @@
-import { randBigInt } from './lib/random'
-import { mod, pow } from './lib/math'
-
-let bn = randBigInt()
-
-let sol = pow(2n,8n,32n)
-
-console.log(sol)
